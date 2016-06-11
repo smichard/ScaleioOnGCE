f7c6dd5801cbff7c231289ec4c918a9b83724f11
Stephan Michard <sstephan.michard@gmail.com>
2016-06-11 19:35:43 +0200

    increased disk size, added line to create a large file at each ScaleIO node in order to be able to add these as devices

--- installScriptNode1.sh
+++ installScriptNode1.sh
@@ -2,8 +2,10 @@
 sudo MDM_ROLE_IS_MANAGER=1 rpm -i EMC-ScaleIO-mdm-2.0-5014.0.el7.x86_64.rpm
 sudo rpm -i EMC-ScaleIO-sds-2.0-5014.0.el7.x86_64.rpm
 sudo MDM_IP=10.132.0.2,10.132.0.3 rpm -i EMC-ScaleIO-sdc-2.0-5014.0.el7.x86_64.rpm
+# Create large file to add as ScaleIO device
+fallocate -l 101G sio1dev1.io
 # ScaleIO Management
-scli --create_mdm_cluster --master_mdm_ip 10.132.0.2 --master_mdm_management_ip 104.155.21.16 --master_mdm_name mdm1 --accept_license
+scli --create_mdm_cluster --master_mdm_ip 10.132.0.2 --master_mdm_management_ip 104.155.6.242 --master_mdm_name mdm1 --accept_license
 scli --login --username admin --password admin
 scli --set_password --old_password admin --new_password Scaleio123
 scli --login --username admin --password Scaleio123
--- installScriptNode2.sh
+++ installScriptNode2.sh
@@ -2,4 +2,5 @@
 sudo MDM_ROLE_IS_MANAGER=1 rpm -i EMC-ScaleIO-mdm-2.0-5014.0.el7.x86_64.rpm
 sudo rpm -i EMC-ScaleIO-sds-2.0-5014.0.el7.x86_64.rpm
 sudo MDM_IP=10.132.0.2,10.132.0.3 rpm -i EMC-ScaleIO-sdc-2.0-5014.0.el7.x86_64.rpm
-
+# Create large file to add as ScaleIO device
+fallocate -l 101G sio2dev1.io 
