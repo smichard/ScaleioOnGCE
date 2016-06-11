ffbb531d8fcffca71cdf5ff5a66d85463af44328
Stephan Michard <sstephan.michard@gmail.com>
2016-06-11 22:59:10 +0200

    fixed adding SDS, now adds SDS with one device per SDS

--- installScriptNode1.sh
+++ installScriptNode1.sh
@@ -5,16 +5,17 @@ sudo MDM_IP=10.132.0.2,10.132.0.3 rpm -i EMC-ScaleIO-sdc-2.0-5014.0.el7.x86_64.r
 # Create large file to add as ScaleIO device
 fallocate -l 101G sio1dev1.io
 # ScaleIO Management
-scli --create_mdm_cluster --master_mdm_ip 10.132.0.2 --master_mdm_management_ip 104.155.6.242 --master_mdm_name mdm1 --accept_license
+scli --create_mdm_cluster --master_mdm_ip 10.132.0.2 --master_mdm_management_ip 104.155.44.255 --master_mdm_name mdm1 --accept_license
 scli --login --username admin --password admin
 scli --set_password --old_password admin --new_password Scaleio123
 scli --login --username admin --password Scaleio123
-scli --add_standby_mdm --new_mdm_ip 10.132.0.3 --mdm_role manager --new_mdm_management_ip 10.132.0.3 --new_mdm_name mdm2
+scli --add_standby_mdm --new_mdm_ip 10.132.0.3 --mdm_role manager --new_mdm_management_ip 104.155.6.242 --new_mdm_name mdm2
 scli --add_standby_mdm --new_mdm_ip 10.132.0.4 --mdm_role tb --new_mdm_name tb1
 scli --switch_cluster_mode --cluster_mode 3_node --add_slave_mdm_name mdm2 --add_tb_name tb1
-# Adding Domain, Pool and SDS
-scli --add_protection_domain --protection_domain_name system
-scli --mdm_ip 10.132.0.2 --add_storage_pool --protection_domain_name system --storage_pool_name pool1
-scli --add_sds --sds_ip 10.132.0.2 --protection_domain_name system --storage_pool_name pool1 --sds_name sds1
-scli --add_sds --sds_ip 10.132.0.3 --protection_domain_name system --storage_pool_name pool1 --sds_name sds2
-scli --add_sds --sds_ip 10.132.0.4 --protection_domain_name system --storage_pool_name pool1 --sds_name sds3
+# Adding Protection Domain and Storage Pool
+scli --add_protection_domain --protection_domain_name pd_system
+scli --mdm_ip 10.132.0.2 --add_storage_pool --protection_domain_name pd_system --storage_pool_name pool1
+# Adding 3 SDS including 1 device each
+scli --mdm_ip 10.132.0.2 --add_sds --sds_ip 10.132.0.2 --protection_domain_name pd_system --storage_pool_name pool1 --device_path /home/superuser/sio1dev1.io --device_name sio1dev1 --sds_name sds1
+scli --mdm_ip 10.132.0.2 --add_sds --sds_ip 10.132.0.3 --protection_domain_name pd_system --storage_pool_name pool1 --device_path /home/superuser/sio2dev1.io --device_name sio2dev1 --sds_name sds2
+scli --mdm_ip 10.132.0.2 --add_sds --sds_ip 10.132.0.4 --protection_domain_name pd_system --storage_pool_name pool1 --device_path /home/superuser/sio3dev1.io --device_name sio3dev1 --sds_name sds3
