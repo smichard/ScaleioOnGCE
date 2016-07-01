5bac45fb4f298d3f9e5ff3516d40bf5feea02791
Stephan Michard <stephan.michard@gmail.com>
2016-07-01 12:45:47 +0200

    setupVagrant.sh - enable silent install for java

--- setupVagrant.sh
+++ setupVagrant.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
-
-	echo "Provisioning virtual machine..."
-	apt-get update
-	echo "Installing git nano jre unzip"
-    apt-get install -y git nano default-jre default-jdk unzip
-	add-apt-repository ppa:webupd8team/java -y
-	apt-get update
-	apt-get install -y oracle-java8-installer
-	echo "Installing gcloud sdk"
-    export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
-    echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list
-    curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-    sudo apt-get update && sudo apt-get install google-cloud-sdk
+echo "Provisioning virtual machine..."
+apt-get update
+echo "Installing git nano unzip"
+apt-get install -y git nano unzip
+echo "Installing java8"
+add-apt-repository ppa:webupd8team/java -y
+apt-get update
+# enable silent install
+echo debconf shared/accepted-oracle-license-v1-1 select true | sudo debconf-set-selections
+echo debconf shared/accepted-oracle-license-v1-1 seen true | sudo debconf-set-selections
+apt-get install -y oracle-java8-installer
+echo "Installing gcloud sdk"
+export CLOUD_SDK_REPO="cloud-sdk-$(lsb_release -c -s)"
+echo "deb http://packages.cloud.google.com/apt $CLOUD_SDK_REPO main" | sudo tee /etc/apt/sources.list.d/google-cloud-sdk.list
+curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+apt-get update
+apt-get install google-cloud-sdk
 	
